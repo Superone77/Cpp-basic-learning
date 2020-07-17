3ea37cf8baf3ebe2ae139bf8ed56c9456473d813
wangchunpeng1 <39366716+wangchunpeng1@users.noreply.github.com>
2020-07-17 21:54:53 +0800

    Update main.cpp
    将原来的非模板函数改成了模板具体化函数实现，并针对类型不匹配（详见代码注释）问题修改了模板函数的声明

--- cpp_primer_plus_code/《C++ Primer Plus（第6版）中文版》习题答案/chapter 8/6/main.cpp
+++ cpp_primer_plus_code/《C++ Primer Plus（第6版）中文版》习题答案/chapter 8/6/main.cpp
@@ -3,9 +3,9 @@
 
 using namespace std;
 
+// 模板函数
 template <typename T>
-const T&
-maxn (const T arr[], unsigned n)
+T maxn (T arr[], unsigned n)
 {
 	unsigned	idxMax = 0;
 	for (unsigned i = 0; i < n; ++i) {
@@ -17,18 +17,25 @@ maxn (const T arr[], unsigned n)
 	return (arr[idxMax]);
 }
 
-const char*
-maxn (const char* arr[], unsigned n)
+
+// 模板具体化  （要使用模板具体化的需要把模板函数和具体化函数的声明一致。否则编译报错：没有与指定类型匹配的模板函数。
+// 这里修改了模板函数声明【原始声明为 template <typename T> const T& maxn (const T arr[], unsigned n)）】
+// 这里猜测不匹配的原因是：模板具体化函数中的const char * 整个被认为是模板函数中的T，所以当在模板函数中再对T进行const限定时，就会认为非模板函数中为非const类型，进而出现不匹配。
+// 即使在模板具体化maxn后的<>中指明类型T仅为char *【template <> const char *maxn<char*>】,仍是会报不匹配错误。 
+// 所以，目前这种写法是将const char *整体作为T，编译通过，运行结果也正确。暂时只理解到这种程度，欢迎大家指正。Thanks♪(･ω･)ﾉ  20200717
+template <> const char* maxn<const char*>(const char *arr[], unsigned n)
 {
-	unsigned	idxMax = 0;
-	for (unsigned i = 0; i < n; ++i) {
-		if (strlen(arr[i]) > strlen(arr[idxMax])) {
-			idxMax = i;
+	unsigned idxMax = 0;
+	if ( n > 0 ) {
+		for ( unsigned i=0; i<n; ++i ) {
+			if ( strlen(arr[i]) > strlen(arr[idxMax]) ) {
+				idxMax = i;
+			}
 		}
 	}
+	return arr[idxMax];
+ }
 
-	return (arr[idxMax]);
-}
 
 int
 main (void)
@@ -46,11 +53,11 @@ main (void)
 	cout << " ----max: " << maxn(dArray, sizeof(dArray)/sizeof(dArray[0])) << endl;
 
 	const char*	szArray[] = {	"aa aa",
-								"dddddddddddd",
-								"",
-								"fffffff ffff",
-								"kk kk",
-	};
+					"dddddddddddd",
+					"",
+					"fffffff ffff",
+					"kk kk",
+				    };
 	for (const auto& e : szArray) {
 		cout << '\"' << e << '\"' << ' ';
 	}
